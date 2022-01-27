7ed6bf961a45471f6724da3e56a8c718643696ee
Jake <2bjake@users.noreply.github.com>
2022-01-27 14:48:40 -0600

    ensure that thrown errors result in an internalServerError

--- Sources/FirstSwiftLambda/Handler.swift
+++ Sources/FirstSwiftLambda/Handler.swift
@@ -2,40 +2,48 @@ import AWSLambdaRuntime
 import AWSLambdaEvents
 import Foundation
 
-let bodyDecoder = JSONDecoder()
-let bodyEncoder = JSONEncoder()
+private let bodyDecoder = JSONDecoder()
+private let bodyEncoder = JSONEncoder()
 
 @main
 struct Handler: AsyncLambdaHandler {
   typealias In = APIGateway.V2.Request
   typealias Out = APIGateway.V2.Response
 
-  struct Input: Codable {
+  private struct Input: Codable {
     let name: String
   }
 
-  struct Output: Codable {
+  private struct Output: Codable {
     let hello: String
   }
 
   init(context: Lambda.InitializationContext) async throws { }
 
-  func handleHello(_ name: String) throws -> Out {
-    let output = try bodyEncoder.encodeAsString(Output(hello: name))
-    return Out(statusCode: .ok, body: output)
+  func handle(event: In, context: Lambda.Context) async -> Out {
+    do {
+      return try await handleInternal(event: event, context: context)
+    } catch {
+      return Out(statusCode: .internalServerError)
+    }
   }
 
-  func handle(event request: In, context _: Lambda.Context) async throws -> Out {
+  private func handleInternal(event request: In, context _: Lambda.Context) async throws -> Out {
     switch (request.context.http.method, request.context.http.path) {
       case (.GET, "/hello"):
-        return try handleHello("world")
+        return handleHello("world")
       case (.POST, "/hello"):
         guard let input = try? bodyDecoder.decode(Input.self, from: request.body ?? "") else {
           return Out(statusCode: .badRequest)
         }
-        return try handleHello(input.name)
+        return handleHello(input.name)
       default:
         return Out(statusCode: .notFound)
     }
+  }
+
+  private func handleHello(_ name: String) -> Out {
+    let output = try! bodyEncoder.encodeAsString(Output(hello: name))
+    return Out(statusCode: .ok, body: output)
   }
 }
